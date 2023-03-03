1a23df390d4e373a09e6d76a0c3c93d7bbe3e564
Jeshon Jackson <jeshonjackson8@gmail.com>
2023-03-03 13:23:41 -0600

    completed project 5

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,39 +1,85 @@
-// Write your helper functions here!
+
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
+    document.getElementById("missionTarget").innerHTML = 
+                `<h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}  </li>
+                    <li>Diameter: ${diameter} </li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance} </li>
+                    <li>Number of Moons: ${moons} </li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">`
+
 }
 
 function validateInput(testInput) {
+    if(testInput === ""){
+        return "Empty";
+    } else if(isNaN(testInput) === true){
+        return "Not a Number";
+    } else if(isNaN(testInput) === false){
+        return "Is a Number";
+    }
    
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+  if(validateInput(pilot) === "Empty" ||
+  validateInput(copilot) === 'Empty'||
+  validateInput(fuelLevel) === 'Empty'||
+  validateInput(cargoMass) === 'Empty'){
+    alert("Make sure to enter valid information for each field!");
+  }
+  if(validateInput(pilot) === "Is a Number" ||
+  validateInput(copilot === "Is a Number") || 
+  validateInput(fuelLevel) === "Not a Number" ||
+  validateInput(cargoMass)=== "Not a Number") {
+    alert("Make sure to enter valid information for each field!");
+  }
+  if(validateInput(pilot) === "Not a Number" || validateInput=(copilot) === "Not a Number") {
+    document.getElementById("pilotStatus").textContent = `Pilot ${pilot} is ready for launch`
+    document.getElementById("copilotStatus").textContent = `Co Pilot ${copilot} is ready for launch`
+    }
+  if(Number(fuelLevel) < 10000){
+    list.style.visibility = "visible";
+    document.getElementById("fuelStatus").innerHTML = `Fuel Level too low for launch`;
+    document.getElementById("launchStatus").innerHTML = `shuttle not Ready for Launch`;
+    document.getElementById("launchStatus").style.color = "rgb(199, 37, 78)";
+} else {
+    document.getElementById("fuelStatus").innerHTML = `Fuel Level high enough for launch`
 }
+if(Number(cargoMass)> 10000){
+    list.style.visibility = "visible";
+    document.getElementById('cargoStatus').innerHTML = `Cargo mass too heavy for launch`;
+    document.getElementById('launchStatus').innerHTML = 'Shuttle not ready for launch';
+    document.getElementById('launchStatus').style.color = "rgb(199,37,78)";
+} else{
+    document.getElementById('cargoStatus').innerHTML = 'Cargo mass low enough for launch';
+}
+if(Number(cargoMass) <= 10000 && Number(fuelLevel) >= 10000){
+    list.style.visibility = "visible";
+    document.getElementById('fuelStatus').innerHTML = 'Fuel level high enough for launch';
+    document.getElementById('cargoStatus').innerHTML = 'Cargo mass low enough for launch';
+    document.getElementById('launchStatus').innerHTML = 'Shuttle is ready for Launch';
+    document.getElementById('launchStatus').style.color = "rgb(65, 159, 106";
+    }
+};
 
 async function myFetch() {
-    let planetsReturned;
+    
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    let planetsReturned = await (await fetch("https://handlers.education.launchcode.org/static/planets.json")).json();
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let index = Math.floor(Math.random() * planets.length);
+    console.log(planets[index]);
+    return planets[index]
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
